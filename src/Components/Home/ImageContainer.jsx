b3662f7945dc3564e78c422829770957c3a6507b
[samiran1605] <[samthemaximum@gmail.com]>
2024-02-28 21:39:14 +0530

    image border

--- src/Components/Home/ImageContainer.jsx
+++ src/Components/Home/ImageContainer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function ImageContainer({imageOrder, imageUrl}) {
   return (
     <>
-    <div className="flex flex-row items-center gap-6 p-24">
-          <img className={`rounded-full object-cover h-40 w-40 ${imageOrder}`} src={imageUrl} alt='dev image' />
+    <div className="flex flex-row items-center gap-6 p-16">
+          <img className={`rounded object-cover h-40 w-40 ${imageOrder}`} src={imageUrl} alt='dev image' />
           <div className="flex flex-col items-start">
             <h2 className='text-4xl text-[#F2E9E4] py-2'>Development</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit a, optio maiores inventore voluptate consequuntur sapiente, atque quis vitae rem magnam deleniti.</p>
